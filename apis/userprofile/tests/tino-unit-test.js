3e33db920567afabb321bae3933e127a0e0573ca
Konstantin Lepeshenkov <kolepes@microsoft.com>
2020-09-22 16:52:21 +0300

    some tests

---
+++ apis/userprofile/tests/tino-unit-test.js
@@ -0,0 +1,17 @@
+'use strict';
+var Test = require('tape');
+var Express = require('express');
+var BodyParser = require('body-parser');
+var Swaggerize = require('swaggerize-express');
+var Path = require('path');
+var Request = require('supertest');
+var Mockgen = require('../data/mockgen.js');
+var Parser = require('swagger-parser');
+/**
+ * Test for /user
+ */
+Test('/user', function (t) {
+
+    t.ok(true, 'Tino test succeeded');
+
+});
